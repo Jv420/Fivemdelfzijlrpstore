2d6cca553fc9f7406d837d50c126bff0b1acc51c
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-10 13:32:00 +0200

    Add FiveM shop bridge manifest

---
+++ fivem/delfzijlrp_shopbridge/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'delfzijlrp_shopbridge'
+author 'Delfzijl RP'
+description 'Stripe/Vercel webshop delivery bridge zonder inkomende poorten'
+version '0.1.0'
+
+server_scripts {
+  'config.lua',
+  'server.lua'
+}
