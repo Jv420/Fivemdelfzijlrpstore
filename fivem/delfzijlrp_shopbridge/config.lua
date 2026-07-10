5ef85e8ce27f43d865380db9796a940a1375a331
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-10 13:32:12 +0200

    Add FiveM bridge config

---
+++ fivem/delfzijlrp_shopbridge/config.lua
@@ -0,0 +1,14 @@
+Config = {}
+
+Config.StoreBaseUrl = 'https://webshop.delfzijlrp.nl'
+Config.BridgeSecret = 'CHANGE_ME_TO_THE_SAME_VALUE_AS_FIVEM_BRIDGE_SECRET'
+Config.PollIntervalSeconds = 45
+
+-- Extra veiligheid: alleen commands met deze prefixes worden uitgevoerd.
+-- Voeg hier alleen commands toe die je zelf vertrouwt.
+Config.AllowedCommandPrefixes = {
+  'add_principal ',
+  'delfzijl_customplate '
+}
+
+Config.Debug = true
